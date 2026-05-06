1c0f2a61938a3eb01db19f506f6d2d33394ffc99
Daniel Medeiros <dmedeiros1209@gmail.com>
2026-05-06 14:32:18 -0300

    Começo do projeto

--- README.md
+++
@@ -1,3 +0,0 @@
-# Começo do estudo de python focado em backend pela Alura
-
-## O código atual é bem simples, só algumas funções para dar os primeiros passos no backend usando Python
--- restaurante/avaliacao.py
+++
@@ -1,5 +0,0 @@
-class Avaliacao:
-    def __init__(self, restaurante, cliente, nota):
-        self._restaurante = restaurante
-        self._cliente = cliente
-        self._nota = nota
